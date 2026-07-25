030c2677ea16a9b66608dd10cc5de4a17733aea2
Daniel <danvalleribeiro@gmail.com>
2026-07-25 10:23:20 -0300

    wip: create placeholder camera script to finish the main scene entry point

--- prototype_level_01.gd
+++ prototype_level_01.gd
@@ -4,10 +4,8 @@ extends BaseLevel
 @onready var player_spawn: Marker2D = $Entities/PlayerSpawn
 @onready var player_camera: Camera2D = $Entities/PlayerCamera
 
-
 func get_default_player_spawn() -> Vector2:
 	return player_spawn.position
-	
+
 func get_player_camera() -> Camera2D:
 	return player_camera
-	
