b851b075c1fe12c3348dca70a9f22a3b4b59932f
Daniel <danvalleribeiro@gmail.com>
2026-07-26 17:44:40 -0300

    added tilemap collision

--- content/core/camera/player_camera.gd
+++ content/core/camera/player_camera.gd
@@ -6,7 +6,7 @@ var target : Node2D = null
 func _physics_process(delta: float) -> void:
 	_follow_target(delta)
 
-func _follow_target(delta : float) -> void:
+func _follow_target(_delta : float) -> void:
 	if(!target):
 		return
 	global_position = target.global_position
